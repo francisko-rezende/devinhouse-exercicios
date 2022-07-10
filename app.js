1118b518bd71101b36937822e88fef0cd634d921
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-07-10 19:21:58 -0300

    termina "[M1S02] Ex 8 - Calcule a diferença de anos e exiba na tela"

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-const contactForm = document.querySelector(".contact-form");
+const currentYear = new Date().getFullYear();
+const userYear = parseInt(prompt("Digite um ano, por favor"));
 
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+const difference = userYear - currentYear;
 
-  const clearInput = () => {
-    contactForm["nome"].value = "";
-    contactForm["email"].value = "";
-    contactForm["descricao"].value = "";
-  };
-
-  const nome = contactForm["nome"].value;
-  const email = contactForm["email"].value;
-  const descricao = contactForm["descricao"].value;
-
-  console.log(`Nome: ${nome} - E-mail: ${email} - Descriçao: ${descricao}`);
-  alert("Formulário enviado com sucesso");
-  clearInput();
-});
+alert(`A diferença entre o ano digitado e o ano atual é de ${difference}`);
