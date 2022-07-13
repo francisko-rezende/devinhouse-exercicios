9e4b5cede90643bda0525701d8d7126df0784790
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-07-12 23:09:21 -0300

    termina "[M1S03] Ex 5 - Projeto: usando arrays"

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const invitedPeople = [
-  { nome: "Gasparino", sobrenome: "Pereira", setor: "camarote", idade: 20 },
-  { nome: "Maria", sobrenome: "Aparecida", setor: "camarote", idade: 16 },
-  {
-    nome: "Rosangela",
-    sobrenome: "dos Santos",
-    setor: "arquibancada",
-    idade: 20,
-  },
-  { nome: "Isaias", sobrenome: "Felizardo", setor: "arquibancada", idade: 16 },
-  { nome: "Anidelci", sobrenome: "Forti", setor: "pista", idade: 20 },
-  { nome: "Janaina", sobrenome: "Matias", setor: "pista", idade: 16 },
+const convidado1 = { nome: "Gasparino", sobrenome: "Pereira", setor: "camarote", idade: 20 }
+const convidado2 = { nome: "Maria", sobrenome: "Aparecida", setor: "camarote", idade: 16 }
+const convidado3 = {
+  nome: "Rosangela",
+  sobrenome: "dos Santos",
+  setor: "arquibancada",
+  idade: 20,
+},
+const convidado4 = { nome: "Isaias", sobrenome: "Felizardo", setor: "arquibancada", idade: 16 }
+const convidado5 = { nome: "Anidelci", sobrenome: "Forti", setor: "pista", idade: 20 }
+const convidado6 = { nome: "Janaina", sobrenome: "Matias", setor: "pista", idade: 16 }
+
+
+const listaDeConvidados = [
+  convidado1,
+  convidado2,
+  convidado3,
+  convidado4,
+  convidado5,
+  convidado6,
 ];
