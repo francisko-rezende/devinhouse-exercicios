90f53345e2365c87058716d5a2e7c6b798360dd7
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-07-20 22:21:06 -0300

    termina "[M1S04] Ex 8 - Número é par ou ímpar"

--- index.js
+++ index.js
@@ -1,110 +1,20 @@
-const contasClientes = [
-  {
-    id: 1,
-    nome: "Cliente 01",
-    saldo: 500,
-    senha: 123
-  },
-  {
-    id: 2,
-    nome: "Cliente 02",
-    saldo: 3000,
-    senha: 456
-  },
-  {
-    id: 3,
-    nome: "Cliente 03",
-    saldo: 5000,
-    senha: 789
-  },
-];
+const form = document.querySelector("form");
 
-const selectContas = document.querySelector("#conta");
-const formulario = document.querySelector("form");
-const inputValor = document.querySelector("#valor");
-const selectOperacao = document.querySelector("#transaction");
+const checkIfEvenOrOdd = (number) => (number % 2 ? "É ímpar" : "É par");
 
-const insereContasNoSelect = () => {
-  contasClientes.forEach((conta) => {
-    selectContas.innerHTML += `<option value="${conta.id}">${conta.id}</option>`;
-  });
-};
-
-const diminuiSaldo = (conta, valorASubtrair) => {
-  conta.saldo -= valorASubtrair;
-};
-
-const saca = (valorSacado, idConta) => {
-  const conta = contasClientes.find((conta) => conta.id === idConta);
-  const saldo = conta.saldo;
-
-  const ehValorInvalido = valorSacado <= 0;
-  const ehSaldoInsuficiente = valorSacado > saldo;
-
-  if (ehValorInvalido) {
-    alert("Valor inválido.");
-    return;
-  }
-
-  if (ehSaldoInsuficiente) {
-    alert(`Saldo insuficiente. Seu saldo é ${conta.saldo}`);
-    return;
-  }
-
-  diminuiSaldo(conta, valorSacado);
-  alert(`Saque realizado com sucesso! Seu novo saldo é ${conta.saldo}`);
-};
-
-const aumentaSaldo = (conta, valorAcrescentado) => {
-  conta.saldo += valorAcrescentado;
-};
-
-const deposita = (valorDepositado, idConta) => {
-  const conta = contasClientes.find((conta) => conta.id === idConta);
-
-  const ehValorInvalido = valorDepositado <= 0;
-
-  if (ehValorInvalido) {
-    alert("Valor inválido.");
-    return;
-  }
-
-  aumentaSaldo(conta, valorDepositado);
-  alert(`O depósito ocorreu com sucesso. Seu saldo é de ${conta.saldo}`);
-};
-
-const executaOperacao = (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const number = Number(document.querySelector("input").value);
 
-  const idConta = Number(selectContas.value);
-  const valor = Number(inputValor.value);
-  const ehSaque = selectOperacao.value === "Saque";
-  const ehDeposito = selectOperacao.value === "Depósito";
-  const contaFoiEncontrada = contasClientes.some((conta) => conta.id === idConta)
-  const senha = document.querySelector('#senha').value
-  const conta = contasClientes.find((conta) => conta.id === idConta);
+  const resultText = document.querySelector('[data-js="result"]');
 
-  const ehASenhaCorreta = conta.senha === senha
-
-  if (!ehASenhaCorreta) {
-    alert('Senha inválida')
-    return
-  }
-
-  if (!contaFoiEncontrada) {
-    alert("Conta não encontrada");
+  if (!resultText) {
+    const resultTextElement = document.createElement("p");
+    resultTextElement.setAttribute("data-js", "result");
+    resultTextElement.textContent = checkIfEvenOrOdd(number);
+    document.body.appendChild(resultTextElement);
     return;
   }
 
-  if (ehSaque) {
-    saca(valor, idConta);
-    return;
-  }
-
-  if (ehDeposito) {
-    deposita(valor, idConta);
-    return;
-  }
-};
-
-formulario.addEventListener("submit", executaOperacao);
+  resultText.textContent = checkIfEvenOrOdd(number);
+});
