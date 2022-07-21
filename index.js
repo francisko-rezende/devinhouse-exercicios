a81aab59a50ef0276baa9f84a2684fd360ff67fa
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-07-21 20:54:36 -0300

    refatora "[M1S04] Ex 10 - Estação do ano"

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
-const dateInput = document.querySelector('input')
+const dateInput = document.querySelector("input");
 
-handleChange = () => {
-  const chosenDate = new Date(dateInput.value).getTime()
-  const isSummer =  chosenDate >= new Date(2021, 11, 22).getTime() && chosenDate <= new Date(2022, 02, 21).getTime()
-  const isFall =  chosenDate >= new Date(2022, 02, 22).getTime() && chosenDate <= new Date(2022, 05, 21).getTime()
-  const isWinter =  chosenDate >= new Date(2022, 05, 22).getTime() && chosenDate <= new Date(2022, 08, 21).getTime()
-  const isSpring =  chosenDate >= new Date(2022, 08, 22).getTime() && chosenDate <= new Date(2022, 11, 21).getTime()
+const insertSeasonMessage = (message, seasonParagraph) =>
+  (seasonParagraph.textContent = message);
 
-  let seasonParagraph = document.querySelector('p')
+handleChange = (ev) => {
+  const chosenDate = new Date(ev.target.value).getTime();
+  const isSummer =
+    chosenDate >= new Date(2021, 11, 22).getTime() &&
+    chosenDate <= new Date(2022, 02, 21).getTime();
+  const isFall =
+    chosenDate >= new Date(2022, 02, 22).getTime() &&
+    chosenDate <= new Date(2022, 05, 21).getTime();
+  const isWinter =
+    chosenDate >= new Date(2022, 05, 22).getTime() &&
+    chosenDate <= new Date(2022, 08, 21).getTime();
+  const isSpring =
+    chosenDate >= new Date(2022, 08, 22).getTime() &&
+    chosenDate <= new Date(2022, 11, 21).getTime();
 
-  const insertSeasonMessage = (message) => seasonParagraph.textContent = message
+  let seasonParagraph = document.querySelector("p");
 
   if (!seasonParagraph) {
-    seasonParagraph = document.createElement('p')
-    document.body.appendChild(seasonParagraph)
+    seasonParagraph = document.createElement("p");
+    document.body.appendChild(seasonParagraph);
   }
-  
+
   if (isSummer) {
-    insertSeasonMessage('É verão 🌞')
-    return
+    insertSeasonMessage("É verão 🌞", seasonParagraph);
+    return;
   }
- 
+
   if (isFall) {
-    insertSeasonMessage('É outono 🍂')
-    return
+    insertSeasonMessage("É outono 🍂", seasonParagraph);
+    return;
   }
-  
+
   if (isWinter) {
-    insertSeasonMessage('É inverno ☃️')
-    return
+    insertSeasonMessage("É inverno ☃️", seasonParagraph);
+    return;
   }
 
   if (isSpring) {
-    insertSeasonMessage('É primavera 🌻')
-    return
+    insertSeasonMessage("É primavera 🌻", seasonParagraph);
+    return;
   }
 
-  seasonParagraph.textContent = 'Não sei a estação dessa data, tenta um dia entre 22 de Dezembro de 2021 e 21 de Dezembro de 2022'
-  
-}
+  seasonParagraph.textContent =
+    "Não sei a estação dessa data, tenta um dia entre 22 de Dezembro de 2021 e 21 de Dezembro de 2022";
+};
+
+dateInput.addEventListener("change", handleChange);
