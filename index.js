4d382184d2411760e9584799ef35ee40c7049ebe
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-08-09 21:21:37 -0300

    termina "[M1S05] - Ex 7 - Instanciando classes"

--- index.js
+++ index.js
@@ -30,3 +30,19 @@ class Pedido {
     return this.listaProdutos.reduce(getTotalPrice, 0);
   }
 }
+
+const sapato = new Produto("nike", 70, true, 5);
+const garrafa = new Produto("squeeze", 20, true, 3);
+const caneca = new Produto("richard", 12, true, 30);
+const monitor = new Produto("samsung", 500, true, 10);
+const mouse = new Produto("magic", 10, false, 10);
+
+const primeiroPedido = new Pedido(20220001, "João");
+const segundoPedido = new Pedido(20220002, "Maria");
+
+primeiroPedido.adicionarProduto(sapato);
+primeiroPedido.adicionarProduto(garrafa);
+primeiroPedido.adicionarProduto(caneca);
+
+segundoPedido.adicionarProduto(monitor);
+segundoPedido.adicionarProduto(mouse);
