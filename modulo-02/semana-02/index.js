21baaf54255388d3485f38de9d1e5f001cb9330c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-10-08 12:22:00 -0300

    [M2S02] Ex 6 - Rotas extras"

--- modulo-02/semana-02/index.js
+++ modulo-02/semana-02/index.js
@@ -9,6 +9,7 @@ let pizzas = [];
 let solicitations = [];
 
 const pizzaSchema = yup.object({
+  id: yup.string(),
   name: yup.string().required("Nome é obrigatório"),
   description: yup.string().required("Descrição é obrigatória"),
   price: yup.number().required("Preço é obrigatório"),
@@ -93,6 +94,26 @@ app.post("/pizzas", async (req, res) => {
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
+});
+
+app.put("/pizzas/:id", async (req, res) => {
+  try {
+    const updatedPizza = { ...req.body };
+    await pizzaSchema.validate(updatedPizza);
+    pizzas = pizzas.map((pizza) => {
+      const isPizzaToUpdate = pizza.id === updatedPizza.id;
+      return isPizzaToUpdate ? updatedPizza : pizza;
+    });
+    return res.status(200).send("Pizza editada com sucesso");
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+app.delete("/pizzas/:id", (req, res) => {
+  const { id } = req.params;
+  pizzas = pizzas.filter((pizza) => pizza.id !== id);
+  return res.status(200).send("Pizza deletada");
 });
 
 app.listen(3333, () => {
