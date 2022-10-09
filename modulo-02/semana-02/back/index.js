882925ae8e5c456ac681f57829a15c96035ab4ce
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-10-09 18:35:48 -0300

    feat: add pizza urls to backend

--- modulo-02/semana-02/back/index.js
+++ modulo-02/semana-02/back/index.js
@@ -14,6 +14,7 @@ let pizzas = [
     description: "Deliciosa pizza de Peperoni",
     price: 123,
     ingredients: ["peperoni", "tomate", "cebola"],
+    imgUrl: "https://i.ibb.co/WKZD7Cy/pepe.webp",
   },
   {
     id: "894f3130-db65-4947-8ac5-97a63ad89b8d",
@@ -21,6 +22,7 @@ let pizzas = [
     description: "Deliciosa pizza de Muçarela",
     price: 123,
     ingredients: ["queijo", "tomate", "cebola"],
+    imgUrl: "https://i.ibb.co/mqzjQ72/mu-a.webp",
   },
   {
     id: "836f545f-d031-4193-8d8b-d045e509309d",
@@ -28,6 +30,7 @@ let pizzas = [
     description: "Deliciosa pizza de Brigadeiro",
     price: 123,
     ingredients: ["brigadeiro"],
+    imgUrl: "https://i.ibb.co/gmVbDm1/briga.webp",
   },
 ];
 let solicitations = [];
