d9cf302e6e9170c9a88b99f210b0f6f8a6af1f09
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-11-02 10:32:55 -0300

    termina [M2S05] Ex 3 - Criar fake db

---
+++ modulo-02/semana-05/cervejaria-api/src/database/database.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import { Beer } from 'src/beer/beer.entity';
+export class Database {
+  private FILENAME = 'beers.json';
+
+  public getBeers(): Beer[] {
+    const beersInFile = fs.readFileSync(this.FILENAME).toString();
+    const beers = JSON.parse(beersInFile);
+    return beers;
+  }
+
+  public saveBeers(beers: Beer[]) {
+    const stringifiedBeers = JSON.stringify(beers);
+    fs.writeFileSync(this.FILENAME, stringifiedBeers);
+  }
+
+  public saveBeer(beer: Beer) {
+    const beers = this.getBeers();
+    const updatedBeers = [...beers, beer];
+    this.saveBeers(updatedBeers);
+  }
+}
