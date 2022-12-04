e3dee4d09b73627850fc65e07fc3870b891f7fc0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-12-04 17:24:12 -0300

    cria entidade para as hashtags

--- modulo-02/semana-10/src/hashtags/entities/hashtag.entity.ts
+++ modulo-02/semana-10/src/hashtags/entities/hashtag.entity.ts
@@ -1,1 +1,13 @@
-export class Hashtag {}
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'hashtags' })
+export class Hashtag {
+  @PrimaryGeneratedColumn()
+  hashtagId: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/\B#\w*[a-zA-Z]+\w*/)
+  hashtag: string;
+}
