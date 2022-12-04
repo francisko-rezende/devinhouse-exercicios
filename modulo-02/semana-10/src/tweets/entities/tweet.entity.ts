c86f4e1cceac92405b0cbc81110afa6052ffa0d1
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-12-04 09:33:07 -0300

    cria versão incial da entidade dos tweets

--- modulo-02/semana-10/src/tweets/entities/tweet.entity.ts
+++ modulo-02/semana-10/src/tweets/entities/tweet.entity.ts
@@ -1,1 +1,18 @@
-export class Tweet {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'tweets' })
+export class Tweet {
+  @PrimaryGeneratedColumn()
+  tweetId: number;
+
+  @Column({ length: 280 })
+  text: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
