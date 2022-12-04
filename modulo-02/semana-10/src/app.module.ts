c2dedc56c70ea24d6a81984dada4cc72bb5ff1e8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-12-04 09:25:32 -0300

    cria nest res para os tweets

--- modulo-02/semana-10/src/app.module.ts
+++ modulo-02/semana-10/src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     UsersModule,
+    TweetsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
