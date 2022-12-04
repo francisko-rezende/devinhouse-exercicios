19d91ca294ec8ee8e882040366c796dc633e4035
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-12-04 09:12:27 -0300

    cria nest res para os usuarios

--- modulo-02/semana-10/src/app.module.ts
+++ modulo-02/semana-10/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
