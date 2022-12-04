80ca949f366cb3578de65e1e932e605156b60779
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-12-04 09:22:33 -0300

    cria migration para a entidade de usuarios

---
+++ modulo-02/semana-10/src/core/database/migrations/1670156249563-init.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class init1670156249563 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
