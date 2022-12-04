e5388d829c92ba1b521cbeebbe902310e9ed95a6
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-12-04 17:16:57 -0300

    cria modulo pras hashtags

--- modulo-02/semana-10/src/app.module.ts
+++ modulo-02/semana-10/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetsModule } from './tweets/tweets.module';
+import { HashtagsModule } from './hashtags/hashtags.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     UsersModule,
     TweetsModule,
+    HashtagsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
