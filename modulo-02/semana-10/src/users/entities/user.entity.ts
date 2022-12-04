de0dddd0f529d957c289c73184519216c2216231
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-12-04 09:20:50 -0300

    termina [M2S10] Ex 1 - Construir entidade para usuários

--- modulo-02/semana-10/src/users/entities/user.entity.ts
+++ modulo-02/semana-10/src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  userId: number;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ length: 50 })
+  user: string;
+
+  @Column({ length: 50 })
+  email: string;
+
+  @Column()
+  photoUrl: string;
+}
